686eb4be7e5e8b309db9f1b66a38fc2647fb0f27
aminlotfi <a.lotfi1201@gmail.com>
2020-04-11 20:34:09 +0430

    add classes Person, Seller, Shopper, Category, Comment, Off, Product, Score.

--- src/main/java/Model/Seller.java
+++ src/main/java/Model/Seller.java
@@ -1,4 +1,16 @@
 package Model;
 
-public class Seller {
+import java.util.ArrayList;
+
+public class Seller extends Person {
+
+    private String factoryName;
+    public ArrayList<Product> allProducts = new ArrayList<Product>();
+    public ArrayList<Off> allOffers = new ArrayList<Off>();
+
+    public Seller(String userName, String firstName, String lastName, String eMail, String phoneNumber, String password, String factoryName) {
+        super(userName, firstName, lastName, eMail, phoneNumber, password);
+        this.factoryName = factoryName;
+        allPerson.add(this);
+    }
 }
