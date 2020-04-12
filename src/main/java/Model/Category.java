d221560d22b4ad9c188af58e805be30063a6f36c
aminlotfi <a.lotfi1201@gmail.com>
2020-04-12 18:30:38 +0430

    edit Category class

--- src/main/java/Model/Category.java
+++ src/main/java/Model/Category.java
@@ -7,9 +7,11 @@ public class Category {
     public static ArrayList<Category> allCategories = new ArrayList<Category>();
     public static ArrayList<Category> subCategories = new ArrayList<Category>();
     private ArrayList<Product> allProducts;
+    //ویژگی های مخصوص
 
-    public Category(String name) {
+
+    public Category(String name, ArrayList<Product> allProducts) {
         this.name = name;
-        allCategories.add(this);
+        this.allProducts = allProducts;
     }
 }
