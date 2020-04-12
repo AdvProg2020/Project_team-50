c5deb0290f123e7024befb0f797aaa9934449411
aminlotfi <a.lotfi1201@gmail.com>
2020-04-12 18:22:58 +0430

    edit Shopper & Person classes

--- src/main/java/Model/Shopper.java
+++ src/main/java/Model/Shopper.java
@@ -1,7 +1,10 @@
 package Model;
 
-public class Shopper extends Person {
+import java.util.ArrayList;
 
+public class Shopper extends Person {
+    private ArrayList<BuyLog> buyLogs = new ArrayList<BuyLog>();
+    private Cart cart;
 
     public Shopper(String userName, String firstName, String lastName, String eMail, String phoneNumber, String password) {
         super(userName, firstName, lastName, eMail, phoneNumber, password);
