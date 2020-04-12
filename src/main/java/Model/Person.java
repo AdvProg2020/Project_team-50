65c567cb12f442ea15000ce895416badcf0452ad
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-04-12 17:17:23 +0430

    Edit Person Class

--- src/main/java/Model/Person.java
+++ src/main/java/Model/Person.java
@@ -1,4 +1,19 @@
 package Model;
 
 public class Person {
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String eMail;
+    private int phoneNumber;
+    private String passWord;
+
+    public Person(String userName, String firstName, String lastName, String eMail, int phoneNumber, String passWord) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.eMail = eMail;
+        this.phoneNumber = phoneNumber;
+        this.passWord = passWord;
+    }
 }
