423754f8e7b4570f76260d12e00da1cb83b411f8
aminlotfi <a.lotfi1201@gmail.com>
2020-04-12 19:08:39 +0430

    edit CodedDiscount class

--- src/main/java/Model/CodedDiscount.java
+++ src/main/java/Model/CodedDiscount.java
@@ -1,23 +1,28 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class CodedDiscount {
     private String discountId;
-    private String startTime;
-    private String endTime;
+    private Date startTime;
+    private Date endTime;
     private double discountRate;
     private double maxDiscountRate;
-    private int usageCount;
     private ArrayList<Person> allPerson = new ArrayList<Person>();
-    private ArrayList<Product> products = new ArrayList<Product>();
+    private HashMap<Person, Integer> usageCount = new HashMap<Person, Integer>();
+    public ArrayList<CodedDiscount> codedDiscounts = new ArrayList<CodedDiscount>();
 
-    public CodedDiscount(String discountId, String startTime, String endTime, double discountRate, double maxDiscountRate, int usageCount) {
+    public CodedDiscount(String discountId, Date startTime, Date endTime, double discountRate, double maxDiscountRate, ArrayList<Person> allPerson) {
         this.discountId = discountId;
         this.startTime = startTime;
         this.endTime = endTime;
         this.discountRate = discountRate;
         this.maxDiscountRate = maxDiscountRate;
-        this.usageCount = usageCount;
+        codedDiscounts.add(this);
+        for (Person person: allPerson) {
+            usageCount.put(person, 0);
+        }
     }
 }
