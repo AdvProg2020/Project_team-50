b85f486a755093c438e3309714528f76ce207791
aminlotfi <a.lotfi1201@gmail.com>
2020-04-12 19:55:24 +0430

    edit Shopper class. delete Cart class.

--- src/main/java/Model/Shopper.java
+++ src/main/java/Model/Shopper.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 
 public class Shopper extends Person {
     private ArrayList<BuyLog> buyLogs = new ArrayList<BuyLog>();
-    private Cart cart;
+    private ArrayList<Product> cart;
 
     public Shopper(String userName, String firstName, String lastName, String eMail, String phoneNumber, String password) {
         super(userName, firstName, lastName, eMail, phoneNumber, password);
+        cart = new ArrayList<Product>();
         allPerson.add(this);
     }
 }
