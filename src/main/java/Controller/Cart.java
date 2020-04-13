81822b5d115b7ac073552b584799b0a34eee737a
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-04-13 18:29:14 +0430

    Edit Cart Class

--- src/main/java/Controller/Cart.java
+++ src/main/java/Controller/Cart.java
@@ -1,4 +1,18 @@
 package Controller;
 
 public class Cart {
+    public void viewShipping() {
+    }
+
+    public void discountCode() {
+    }
+
+    public void finalApproval() {
+    }
+
+    public void confirmationAmountDeduction() {
+    }
+
+    public void backToMainPage() {
+    }
 }
