f509ad561052e711709737b3d5a5f71b1524de09
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-04-14 21:36:07 +0430

    Edit CreateNewDiscountCode Class

--- src/main/java/Controller/CreateNewDiscountCode.java
+++ src/main/java/Controller/CreateNewDiscountCode.java
@@ -1,5 +1,6 @@
 package Controller;
 
 public class CreateNewDiscountCode {
-
+    public String discountCode(String discountCode, int startTime, int endTime, float discountRate, int repeatedTimes) {
+    }
 }
