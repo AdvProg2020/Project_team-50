f35b9ff405b245af71b439920dffcf1c8e3f6243
aminlotfi <a.lotfi1201@gmail.com>
2020-04-14 22:43:46 +0430

    add SellerController class in Controller package and add some methods in Seller and Shopper Controller.

---
+++ src/main/java/Controller/ShopperController.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import Model.Product;
+import Model.Shopper;
+
+public class ShopperController {
+    public static void changePersonalInfo(Shopper shopper, String field, String newInfo){
+        //TODO
+    }
+
+    public static void showProducts(){
+        //TODO
+    }
+
+    public static void  addProduct(String productId){
+        //TODO
+    }
+
+    public static void deleteProduct(String productId){
+        //TODO
+    }
+
+    public static void compareTwoProduct(String productId1, String productId2){
+        //TODO
+    }
+
+    public static void showCredit(Shopper shopper){
+        //TODO
+    }
+
+    public static void purchase(Shopper shopper){
+        //TODO
+    }
+}
