4311469d0b8db267333e6adf794b9fb711df153c
aminlotfi <a.lotfi1201@gmail.com>
2020-04-15 18:13:00 +0430

    add a ProductsController Class and some methods.

---
+++ src/main/java/Controller/ProductsController.java
@@ -0,0 +1,41 @@
+package Controller;
+
+import Model.Category;
+import Model.Product;
+
+import java.util.ArrayList;
+
+public class ProductsController {
+    public ArrayList<Category> showCategories(){
+        //TODO
+    }
+
+    public ArrayList<Product> showProducts(){
+        //TODO
+    }
+
+    public ArrayList<String> addFilter(String filterType){
+        //TODO
+    }
+
+    public ArrayList<String> deleteFilter(String filterType){
+        //TODO
+    }
+
+    public void sortByRate(){
+        //TODO
+    }
+
+    public void sortByView(){
+        //TODO
+    }
+
+    public void sortByReleaseTime(){
+        //TODO
+    }
+    public void deleteSort(){
+        //TODO
+    }
+
+
+}
