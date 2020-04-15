b604a3f19f3e6f73e553cd665a4e8cdc2c4927e0
aminlotfi <a.lotfi1201@gmail.com>
2020-04-15 15:56:30 +0430

    add Cart class with some methods.

---
+++ src/main/java/Model/Cart.java
@@ -0,0 +1,23 @@
+package Model;
+
+import java.util.HashMap;
+
+public class Cart {
+    private HashMap<Product, Integer> products = new HashMap<Product, Integer>();
+
+    public void addProduct(Product product){
+        //TODO
+    }
+
+    public void deleteProduct(Product product){
+        //TODO
+    }
+
+    public HashMap<Product, Integer> getProducts() {
+        return products;
+    }
+
+    public double getTotalCartPrice(){
+        //TODO
+    }
+}
