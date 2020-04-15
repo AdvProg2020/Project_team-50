9ad12161f02cf0a774fbad6290841a47eb9ab220
aminlotfi <a.lotfi1201@gmail.com>
2020-04-15 17:36:12 +0430

    add EntryController Class and some methods

---
+++ src/main/java/Controller/EntryController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+public class EntryController {
+    public void logIn(String userName, String password){
+        //TODO
+    }
+
+    public void logOut(String userName){
+        //TODO
+    }
+
+    public boolean isLogIn(String userName){
+        //TODO
+    }
+
+    public boolean isTherePersonWithUserName(String userName){
+        //TODO
+    }
+}
