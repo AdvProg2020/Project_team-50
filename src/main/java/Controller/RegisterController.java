e05ae65d609badf6668c21b068e6dc2aeb316838
aminlotfi <a.lotfi1201@gmail.com>
2020-04-15 16:27:09 +0430

    add RegisterController Class and some methods

---
+++ src/main/java/Controller/RegisterController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import Model.Admin;
+
+public class RegisterController {
+    public void registerAdmin(String userName, String firstName, String lastName, String eMail, String phoneNumber, String password){
+        //TODO
+    }
+
+    public void registerSeller(String userName, String firstName, String lastName, String eMail, String phoneNumber, String password, String factoryName){
+        //TODO
+    }
+
+    public void registerShopper(String userName, String firstName, String lastName, String eMail, String phoneNumber, String password){
+        //TODO credit = 0
+    }
+
+    public static boolean canAddNewAdmin(Admin admin){
+        //TODO
+    }
+}
