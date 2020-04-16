119d4010dfcfa75f71815f1199d9ee252b36558e
aminlotfi <a.lotfi1201@gmail.com>
2020-04-16 19:25:28 +0430

    add methods to CartController class.

--- src/main/java/controller/CartController.java
+++ src/main/java/controller/CartController.java
@@ -1,4 +1,33 @@
 package controller;
 
+import model.Cart;
+import model.Product;
+import model.account.Seller;
+
+import java.util.ArrayList;
+
 public class CartController {
+    private void addProductToCart(Product product, Seller seller){
+        //TODO
+    }
+
+    private void deleteProductFromCart (Product product){
+        //TODO
+    }
+
+    private void clearCart(){
+        //TODO
+    }
+
+    public boolean isProductInCart(Product product){
+        //TODO
+    }
+
+    public void changeNumberOfProduct(Product product, int newValue){
+        //TODO
+    }
+
+    public Cart showCart(){
+        //TODO
+    }
 }
