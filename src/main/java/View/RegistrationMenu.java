20e74797b907bde512bcd4c5ef355ffe12f75dbc
aminlotfi <a.lotfi1201@gmail.com>
2020-04-16 21:07:57 +0430

    add RegistrationMenu class and edit LoginMenu class methods.

---
+++ src/main/java/View/RegistrationMenu.java
@@ -0,0 +1,12 @@
+package view;
+
+public class RegistrationMenu extends Menu{
+    public RegistrationMenu(Menu parentMenu) {
+        super("RegistrationMenu", parentMenu);
+        this.setSubmenus(submenus);
+    }
+
+    public void register(String type, String userName){
+        //TODO
+    }
+}
