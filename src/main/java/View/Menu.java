96aba5d8970920525c17dd0f2b92306805b45634
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-02 12:57:40 +0430

    Edit show & execute public in package view.

--- src/main/java/View/Menu.java
+++ src/main/java/View/Menu.java
@@ -6,12 +6,16 @@ import java.util.Scanner;
 public class Menu {
     private String name;
     protected Menu parentMenu;
-    protected HashMap<Integer, Menu> submenus;
-    public static Scanner scanner;
+    protected HashMap<Integer, Menu> subMenus;
+    protected static Scanner scanner;
 
     public Menu(String name, Menu parentMenu) {
         this.name = name;
         this.parentMenu = parentMenu;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public static void setScanner(Scanner scanner) {
@@ -23,14 +27,34 @@ public class Menu {
     }
 
     public void setSubmenus(HashMap<Integer, Menu> submenus) {
-        this.submenus = submenus;
+        this.subMenus = submenus;
     }
 
     public void show(){
-        //TODO
+        System.out.println(this.name + ":");
+        for (Integer menuNumber : subMenus.keySet()) {
+            System.out.println(menuNumber + "-" + subMenus.get(menuNumber).getName());
+        }
+        if (this.parentMenu != null) {
+            System.out.println((this.subMenus.size() + 1) + "-Back");
+        } else {
+            System.out.println((this.subMenus.size() + 1) + "-Exit");
+        }
     }
 
     public void execute(){
-        //TODO
+        Menu nextMenu = null;
+        int input = Integer.parseInt(scanner.nextLine());
+        if (input == this.subMenus.size() + 1) {
+            if (this.parentMenu == null) {
+                System.exit(1);
+            } else {
+                nextMenu = this.parentMenu;
+            }
+        } else {
+            nextMenu = subMenus.get(input);
+        }
+        nextMenu.show();
+        nextMenu.execute();
     }
 }
