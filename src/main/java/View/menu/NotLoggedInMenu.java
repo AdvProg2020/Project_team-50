7c6bbd68c23d7b5280d34bf136942d0a722991c3
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-06 05:20:32 +0430

    Edit NotLoggedInMenu class.

--- src/main/java/View/menu/NotLoggedInMenu.java
+++ src/main/java/View/menu/NotLoggedInMenu.java
@@ -6,4 +6,18 @@ public class NotLoggedInMenu extends Menu {
     public NotLoggedInMenu(Menu parentMenu) {
         super("NotLoggedInMenu", parentMenu);
     }
+
+    private Menu RegistrationPanelMenu() {
+        return new Menu("RegistrationPanel", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
 }
