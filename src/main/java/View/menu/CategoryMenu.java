56c0d82313dd41f0ba8ba1b80e16d4a58dc638a4
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-06 05:36:07 +0430

    Edit CategoryMenu class.

--- src/main/java/View/menu/CategoryMenu.java
+++ src/main/java/View/menu/CategoryMenu.java
@@ -6,4 +6,32 @@ public class CategoryMenu extends Menu {
     public CategoryMenu(Menu parentMenu) {
         super("Category", parentMenu);
     }
+
+    private Menu ListOfProductMenu() {
+        return new Menu("ListOfProduct", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu SearchMenu() {
+        return new Menu("Search", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
 }
