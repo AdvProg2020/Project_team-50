b72680a35147dc5c460ad6f84a377319a6e9bb59
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-06 04:54:56 +0430

    Add & edit NotLoggedInMenu class.

---
+++ src/main/java/View/menu/NotLoggedInMenu.java
@@ -0,0 +1,9 @@
+package view.menu;
+
+import view.Menu;
+
+public class NotLoggedInMenu extends Menu {
+    public NotLoggedInMenu(Menu parentMenu) {
+        super("NotLoggedInMenu", parentMenu);
+    }
+}
