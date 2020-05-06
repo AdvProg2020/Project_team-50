0d077155539911135e8e5ea7bbed5362ea286bcb
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-06 04:51:52 +0430

    Add NotLoggedIn class.

---
+++ src/main/java/View/menu/NotLoggedIn.java
@@ -0,0 +1,9 @@
+package view.menu;
+
+import view.Menu;
+
+public class NotLoggedIn extends Menu {
+    public NotLoggedIn(String name, Menu parentMenu) {
+        super(name, parentMenu);
+    }
+}
