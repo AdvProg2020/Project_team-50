5a6e1e71aeb0498ccbee7f42d6dc82cae9cd4adc
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-06 05:02:44 +0430

    Edit UserMenu class.

--- src/main/java/View/UserMenu.java
+++ src/main/java/View/UserMenu.java
@@ -1,9 +1,17 @@
 package view;
 
+import view.menu.LoggedInMenu;
+import view.menu.NotLoggedInMenu;
+
+import java.util.HashMap;
+
 public class UserMenu extends Menu {
     public UserMenu(Menu parentMenu) {
         super("UserMenu", parentMenu);
-        this.setSubmenus(submenus);
+        HashMap<Integer, Menu> subMenus = new HashMap<Integer, Menu>();
+        subMenus.put(1, new LoggedInMenu(this));
+        subMenus.put(2, new NotLoggedInMenu(this));
+        this.setSubmenus(subMenus);
     }
 
     @Override
--- src/main/java/View/menu/NotLoggedIn.java
+++
@@ -1,9 +0,0 @@
-package view.menu;
-
-import view.Menu;
-
-public class NotLoggedIn extends Menu {
-    public NotLoggedIn(String name, Menu parentMenu) {
-        super(name, parentMenu);
-    }
-}
