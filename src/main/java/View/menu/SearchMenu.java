12a12b33973249b39fab581675c14c5be57e41ae
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-08 05:11:00 +0430

    Edit SearchMenu class.

--- src/main/java/View/menu/SearchMenu.java
+++ src/main/java/View/menu/SearchMenu.java
@@ -1,4 +1,37 @@
 package view.menu;
 
-public class SearchMenu {
+import view.Menu;
+
+public class SearchMenu extends Menu {
+    public SearchMenu(Menu parentMenu) {
+        super("Search", parentMenu);
+    }
+
+    private Menu GeneralFeaturesMenu() {
+        return new Menu("GeneralFeatures", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
+
+    private Menu CategoryFeaturesMenu() {
+        return new Menu("CategoryFeatures", this) {
+            @Override
+            public void show() {
+                super.show();
+            }
+
+            @Override
+            public void execute() {
+                super.execute();
+            }
+        };
+    }
 }
