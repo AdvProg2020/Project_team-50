accae82ee97729bb08e9d9fbcbb5d4cf3e19fc8f
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-08 05:58:47 +0430

    Edit ConsoleCommands class.

--- src/main/java/View/ConsoleCommands.java
+++ src/main/java/View/ConsoleCommands.java
@@ -1,4 +1,16 @@
 package view;
 
+import java.util.regex.Pattern;
+
 public enum ConsoleCommands {
+    ;
+    private final Pattern commandsPattern;
+
+    ConsoleCommands(String commandsPatternString) {
+        this.commandsPattern = Pattern.compile(commandsPatternString);
+    }
+
+    public Pattern getCommandsPattern() {
+        return commandsPattern;
+    }
 }
