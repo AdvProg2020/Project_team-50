1c94567b0fbf447ea45ba9f0cd1923bbed0c1f14
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-13 00:07:09 +0430

    Edit ConsoleCommands class.

--- src/main/java/View/ConsoleCommands.java
+++ src/main/java/View/ConsoleCommands.java
@@ -12,9 +12,22 @@ public enum ConsoleCommands {
     MANAGER("(?i)manage\\s+users"),
     VIEW("(?i)view\\s+(\\w+)"),
     DELETE_USER("(?i)delete\\s+(\\w+)"),
-    MANAGER_PROFILE("(?i)create\\s+manager\\s+profile");
-
-
+    MANAGER_PROFILE("(?i)create\\s+manager\\s+profile"),
+    MANAGE_PRODUCTS("(?)manage\\S+all\\S+products"),
+    REMOVE_PRODUCTS("(?)remove\\s+(\\w+)"),
+    CREATE_DISCOUNT_CODE("(?)create\\s+discount\\s+code"),
+    DISCOUNT_CODE("(?)view\\s+discount\\s+code"),
+    VIEW_DISCOUNT_CODE("(?)view\\s+discount\\s+code\\s+(\\w+)"),
+    EDIT_DISCOUNT_CODE("(?)edit\\s+discount\\s+code\\s+(\\w+)"),
+    REMOVE_DISCOUNT_CODE("(?)remove\\s+discount\\s+code\\s+(\\w+)"),
+    MANAGE_REQUESTS("(?)manage\\s+requests"),
+    REQUESTS_DETAILS("(?)details\\s+(\\w+)"),
+    ACCEPT_REQUESTS("(?)accept\\s+(\\w+)"),
+    DECLINE_REQUESTS("(?)decline\\s+(\\w+)"),
+    MANAGE_CATEGORIES("(?)manage\\s+categories"),
+    EDIT_CATEGORIES("(?)edit\\s+(\\w+)"),
+    ADD_CATEGORIES("(?)add\\s+(\\w+)"),
+    REMOVE_CATEGORIES("(?)remove\\s+(\\w+)");
     private final Pattern commandsPattern;
 
     ConsoleCommands(String commandsPatternString) {
