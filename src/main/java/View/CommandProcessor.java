fff822ff03a0fedbaec2413549c2841b6819f8c1
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-13 15:11:08 +0430

    Edit CommandProcessor class.

--- src/main/java/View/CommandProcessor.java
+++ src/main/java/View/CommandProcessor.java
@@ -3,6 +3,34 @@ package view;
 import java.util.Scanner;
 
 public class CommandProcessor {
-    public void Start() {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public void start() {
+        while (true) {
+            String command = scanner.nextLine().trim();
+            if (ConsoleCommands.EXIT.getStringMatcher(command).matches()) {
+                break;
+            } else if (ConsoleCommands.REGISTER.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.LOGIN.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.VIEW_MANAGER_PERSONAL_INFO.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.EDIT_MANAGER_PERSONAL_INFO.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.MANAGING.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.VIEW_USER.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.DELETE_USER.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.MANAGER_PROFILE.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.MANAGE_PRODUCTS.getStringMatcher(command).matches()) {
+                //TODO
+            } else if (ConsoleCommands.REMOVE_PRODUCTS.getStringMatcher(command).matches()) {
+                //TODO
+            }
+        }
     }
 }
