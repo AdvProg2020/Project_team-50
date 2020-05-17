8b0b1974e859f6886c98e0a6d59b28f48b745789
Mehran Khaksar <mewhrankhaksar@yahoo.com>
2020-05-17 11:32:17 +0430

    Edit CommandProcessor class.

--- src/main/java/View/CommandProcessor.java
+++ src/main/java/View/CommandProcessor.java
@@ -29,22 +29,26 @@ public class CommandProcessor {
             } else if (ConsoleCommands.VIEW_MANAGER_PERSONAL_INFO.getStringMatcher(command).matches()) {
                 Matcher matcher = ConsoleCommands.VIEW_MANAGER_PERSONAL_INFO.getStringMatcher(command);
                 if (matcher.find()) {
-                    //TODO
+                    controller.AdminController adminController = null;
+                    adminController.showPersonalInfo();
                 }
             } else if (ConsoleCommands.EDIT_MANAGER_PERSONAL_INFO.getStringMatcher(command).matches()) {
                 Matcher matcher = ConsoleCommands.EDIT_MANAGER_PERSONAL_INFO.getStringMatcher(command);
                 if (matcher.find()) {
                     String field = matcher.group(1);
+                    //TODO
                 }
             } else if (ConsoleCommands.MANAGING.getStringMatcher(command).matches()) {
                 Matcher matcher = ConsoleCommands.MANAGING.getStringMatcher(command);
                 if (matcher.find()) {
-                    //TODO
+                    controller.AdminController adminController = null;
+                    adminController.showAllUsers();
                 }
             } else if (ConsoleCommands.VIEW_USER.getStringMatcher(command).matches()) {
                 Matcher matcher = ConsoleCommands.VIEW_USER.getStringMatcher(command);
                 if (matcher.find()) {
                     String userName = matcher.group(1);
+                    
                 }
             } else if (ConsoleCommands.DELETE_USER.getStringMatcher(command).matches()) {
                 Matcher matcher = ConsoleCommands.DELETE_USER.getStringMatcher(command);
@@ -110,6 +114,46 @@ public class CommandProcessor {
                 Matcher matcher = ConsoleCommands.DECLINE_REQUESTS.getStringMatcher(command);
                 if (matcher.find()) {
                     String request = matcher.group(1);
+                }
+            } else if (ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    //TODO
+                }
+            } else if (ConsoleCommands.EDIT_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.EDIT_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String category = matcher.group(1);
+                }
+            } else if (ConsoleCommands.ADD_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.ADD_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String category = matcher.group(1);
+                }
+            } else if (ConsoleCommands.REMOVE_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.REMOVE_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String category = matcher.group(1);
+                }
+            } else if (ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String discountCode = matcher.group(1);
+                }
+            } else if (ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String discountCode = matcher.group(1);
+                }
+            } else if (ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String discountCode = matcher.group(1);
+                }
+            } else if (ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command).matches()) {
+                Matcher matcher = ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command);
+                if (matcher.find()) {
+                    String discountCode = matcher.group(1);
                 }
             } else if (ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command).matches()) {
                 Matcher matcher = ConsoleCommands.MANAGE_CATEGORIES.getStringMatcher(command);
